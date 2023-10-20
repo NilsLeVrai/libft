4ba26e61d6bb0feaaf38e04209bc8938f03dc450
Nils Abraham <44902217+NilsLeVrai@users.noreply.github.com>
2023-10-20 13:54:22 +0200

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -8,12 +8,10 @@ int ft_strncmp(const char *s1, const char *s2, size_t n)
     size_t i;
 
     i = 0;
-    while (s1[i] == s2[i] && i < n)
-    {
-        if (n == 0)
+    if (n == 0)
             return (0);
+    while (s1[i] == s2[i] && i < n)
         i++;
-    }
     return (s1[i] - s2[i]);
 }
 
