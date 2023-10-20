4caacda3ef859355cb1025c352952331b3cac66b
Nils Abraham <44902217+NilsLeVrai@users.noreply.github.com>
2023-10-20 13:49:19 +0200

    Add files via upload

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,24 +1,31 @@
-#include <stddef.h>
 
-int		ft_strncmp(const char *s1, const char *s2, size_t n)
-{
-	
-}
-	
-#include <string.h>
+#include <stddef.h>
 #include <stdio.h>
+#include <string.h>
+
+int ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+    size_t i;
+
+    i = 0;
+    while (s1[i] == s2[i] && i < n)
+    {
+        if (n == 0)
+            return (0);
+        i++;
+    }
+    return (s1[i] - s2[i]);
+}
+
 int main ()
 {
-	char *s1 = "ouais la team";	
-	char *s2 = "oais la team";
-
-	char *s3 = "Bad omdns";
-	char *s4 = "Bzd omens";
-	printf("Pas moi%d\n", strncmp(s1, s2, 6));
-	printf("Moi%d\n", ft_strncmp(s1, s2, 6));
-	printf("Pas moi%d\n", strncmp(s3, s4, 8));
-	printf("Moi%d\n", ft_strncmp(s3, s4, 8));
+    int res;
+    size_t  i;
+    char tab[20] = "ouais la team";
+    char tab2[20] = "ouais la team";
+    i = 0;
+    res = strncmp(tab, tab2, i);
+    printf("Pas moi :( %d\n", res);
+    res = ft_strncmp(tab, tab2, i);
+    printf("Moi :) %d", res);
 }
-
-
-
