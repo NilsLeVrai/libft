ba0ced3936b72a2ce60259f4735203d2c72aca37
NilsLeVrai <nilslucas@wanadoo.fr>
2023-10-24 16:11:40 +0200

    part2 fd

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -1,32 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: niabraha <niabraha@42.fr>                  +#+  +:+       +#+        */
+/*   By: niabraha <niabraha@students.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/18 15:21:24 by niabraha          #+#    #+#             */
-/*   Updated: 2023/10/20 15:33:17 by niabraha         ###   ########.fr       */
+/*   Created: 2023/10/24 15:42:20 by niabraha          #+#    #+#             */
+/*   Updated: 2023/10/24 15:42:20 by niabraha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dest, const void *src, size_t n)
+void    ft_putchar_fd(char c, int fd)
 {
-	char		*dest_ptr;
-	const char	*src_ptr;
-
-	dest_ptr = dest;
-	src_ptr = src;
-	if (dest > src)
-	{
-		while (n--)
-		{
-			*dest_ptr = *src_ptr;
-			dest_ptr++;
-			src_ptr++;
-		}
-	}
-	return (dest);
+    if (fd < 0)
+        return ;
+    write(fd, &c, 1);
 }
---
+++ ft_strjoin.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: niabraha <niabraha@students.42.fr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/24 16:03:26 by niabraha          #+#    #+#             */
+/*   Updated: 2023/10/24 16:03:26 by niabraha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char    *ft_strcat(char *dest, char *src)
+{
+    int i;
+    int j;
+
+    i = 0;
+    j = 0;
+    while (dest[i] != '\0')
+        i++;
+    while (src[j] != '\0')
+    {
+        dest[i] = src[j];
+        j++;
+        i++;
+    }
+    dest[i] = '\0';
+    return (dest);
+}
+
+int ft_size_malloc(int nb, char **strs, char *sep)
+{
+    int i;
+    int malloclen;
+
+    i = 0;
+    malloclen = 0;
+    while (i < nb)
+    {
+        malloclen += ft_strlen(strs[i]);
+        i++;
+    }
+    malloclen += ft_strlen(sep) * (nb - 1);
+    return (malloclen + 1);
+}
+
+char    *ft_strjoin(char const *s1, char const *s2)
+{
+    char    *mallocrane;
+    int     ballec;
+    int     i;
+
+    i = 0;
+    ballec = ft_size_malloc(size, s1, s2);
+    if (!s1 || !s2)
+        return (NULL);
+    if (size == 0)
+    {
+        mallocrane = (char *) malloc (1 * sizeof(char));
+        mallocrane[0] = '\0';
+        return (mallocrane);
+    }
+    mallocrane = (char *) malloc (ballec * sizeof(char));
+    while (i < size)
+    {
+        ft_strcat(mallocrane, strs[i]);
+        if (i < size - 1)
+            ft_strcat(mallocrane, sep);
+        i++;
+    }
+    mallocrane[ballec - 1] = '\0';
+    return (mallocrane);
+}
