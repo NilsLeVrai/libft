81b990735adc3ffcb3f78c306c43169eb3dc074c
Nils Abraham <44902217+NilsLeVrai@users.noreply.github.com>
2023-10-26 17:57:20 +0200

    Update ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -14,67 +14,73 @@
 
 int	malloc_size(int n)
 {
-	int i;
-	long	nb;
-
-	i = 0;
-	nb = n;
-	if (nb == 0)
-		return (1);
-	if (nb < 0)
-		nb *= -1;
-	while(n > 0)
-	{
-		n /= 10;
-		i++;
-	}
-	return (i);
+{
+        int     i;
+        long    nb;
+        
+        i = 0; 
+        nb = n; 
+        if (nb == 0)
+                return (1);
+        if (nb < 0)
+                nb *= -1;
+        while(nb > 0) 
+        {       
+                nb /= 10;
+                i++;
+        }
+        return (i);
 }
 
-int	power(int power)
-{
-	int	i;
-	int	nb;
-
-	i = 0;
-	nb = 1;
-	while (i < power)
-	{
-		nb *= 10;
-		i++;
-	}
-	return (nb);
+int     power(int power)
+{       
+        int     i;
+        int     nb;
+ 
+        i = 0;
+        nb = 1;
+        while (i < power)
+        {       
+                nb *= 10;
+                i++;
+        }
+        return (nb);
 }
 
-char	*ft_itoa(int n)
+char    *ft_itoa(int n)
 {
-	char 	*tab;
-	int	i;
-	int 	len;
-	int 	base;
-	long	nb;
+        char    *tab;
+        int     i;
+        int     len;
+        int     base;
+        long    nb;
+        
+        i = 0;
+        nb = n;
+        len = malloc_size(n);
+        base = power(len - 1);
+        tab = (char *) malloc(sizeof(char) * (len + 1));
+        if (!tab)
+                return (NULL);
+        if (nb < 0)
+        {       
+                tab[0] = '-';
+                nb *= -1;
+        }
+        while (i < len)
+        {
+                tab[i++] = ((nb / base) + '0');
+                nb %= base;
+                base /= 10;
+        }
+        tab[i] = '\0';
+        return (tab);
+}
 
-	i = 0;
-	len = malloc_size(n);
-	base = power(len - 1);
-	tab = (char *) malloc(sizeof(char) * (len + 1));
-	nb = n;
-	if (!tab)
-		return (NULL);
-	while (i < len)
-	{
-		tab[i++] = ((n / base) + '0');
-		n %= base;
-		base /= 10;
-	}
-	tab[i] = '\0';
-	return (tab);
-} 
-
-/* int main()
+int main()
 {
-	int		i;
+        int             i;
 
-	i = 0;
-	printf("%s", ft_itoa(i));
-} */
+        i = -27641;
+        printf("%s", ft_itoa(i));
+}
