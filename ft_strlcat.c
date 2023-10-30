b134f50fb1e5f04dd975bc8421aa3554326d112b
NilsLeVrai <nilslucas@wanadoo.fr>
2023-10-31 00:00:32 +0100

    maudit matou

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -12,7 +12,36 @@
 
 #include "libft.h"
 
-/* size_t	ft_strlcat(char *dst, const char *src, size_t n)
+size_t	ft_strlcat(char *dst, const char *src, size_t n)
 {
+	size_t	i;
+	size_t	len_dest;
+	size_t	len_src;
 
-} */
+	i = 0;
+	len_dest = ft_strlen(dst);
+	len_src = ft_strlen(src);
+	if (n < len_dest)
+		len_src += n;
+	else
+		len_src += len_dest;
+	while (n > len_dest + 1 && src[i])
+	{
+		dst[len_dest] = src[i];
+		len_dest++;
+		i++;
+	}
+	if (n >= len_dest)
+		dst[len_dest] = '\0';
+	return (len_src);
+}
+
+/*
+int main ()
+{
+	char *len_dest = "zz,";
+	char *len_src = "zlzecdfvgbhnj";
+	size_t n = 3;
+	printf("%ld", strlcat(len_dest, len_src, n));
+}
+*/
