65e510510a081fabbd77b112fbb03c6d447d55c3
Nils Abraham <44902217+NilsLeVrai@users.noreply.github.com>
2023-11-01 23:36:25 +0100

    Add files via upload

---
+++ ft_ptrf.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include"ft_tolower.c"
+#include"ft_toupper.c"
+
+void iterer(char* s, int (*ptr)(int))
+{
+	while (*s)
+	{
+		printf("%c", (*ptr)(*s));
+		s++;
+	}
+	printf("\n");
+}
+int main(int argc, char** argv)
+{
+	iterer("abcDEF'134 xyzWVU",&ft_tolower);
+	iterer("abcDEF'134 xyzWVU",&ft_toupper);
+	return 0;
+}
+
