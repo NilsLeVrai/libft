9a0dd18fc0e4721c8be9572e1b7349b7b8d31756
nils <nilslucas@wanadoo.fr>
2023-11-07 14:44:11 +0100

    lstadd

---
+++ ft_lstadd_back_bonus.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: niabraha <niabraha@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/07 14:14:29 by niabraha          #+#    #+#             */
+/*   Updated: 2023/11/07 14:43:42 by niabraha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*var;
+
+	var = *lst;
+	if (*lst == NULL)
+	{
+		*lst = new;
+		return ;
+	}
+	while (var->next != NULL)
+		var = var->next;
+	var->next = new;
+}
+
